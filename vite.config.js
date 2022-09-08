d093b01f08379f556ee1ad5a34605e4db84b7700
Luis Souza <appdevcanada@gmail.com>
2022-09-07 20:52:50 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // resolve: {
-  //   alias:
-  //     {
-  //       find: "components",
-  //       replacement: resolve(__dirname, "src/components"),
-  //     }
-  // },
   plugins: [react()],
   base: "/react-vite/",
   build: {
